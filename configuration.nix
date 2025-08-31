5c2af35208eb4f5e2a641c040ac7917652e1b476
shyam <shyam163@gmail.com>
2025-08-31 16:27:37 +0530

    Fix flake configuration

--- configuration.nix
+++ configuration.nix
@@ -36,42 +36,16 @@
   # Enable flakes and new nix command
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   
-nixarr = {
+  nixarr = {
     enable = true;
-    # These two values are also the default, but you can set them to whatever
-    # else you want
-    # WARNING: Do _not_ set them to `/home/user/whatever`, it will not work!
     mediaDir = "/media";
-    stateDir = "/media/.state/nixarr";
-
-    vpn = {
-      enable = false;
-      # WARNING: This file must _not_ be in the config git directory
-      # You can usually get this wireguard file from your VPN provider
-      wgConf = "/data/.secret/wg.conf";
-    };
-
-    jellyfin = {
-      enable = true;
-      };
-    };
-
-    transmission = {
-      enable = true;
-      vpn.enable = false;
-    };
-
-    # It is possible for this module to run the *Arrs through a VPN, but it
-    # is generally not recommended, as it can cause rate-limiting issues.
-    bazarr.enable = true;
-    lidarr.enable = false;
-    prowlarr.enable = true;
+    
+    jellyfin.enable = true;
+    transmission.enable = true;
     radarr.enable = true;
-    readarr.enable = true;
     sonarr.enable = true;
-    jellyseerr.enable = true;
+    prowlarr.enable = true;
   };
-
 
  #mount media flder 
   fileSystems."/media" = {
--- flake.nix
+++ flake.nix
@@ -1,27 +1,20 @@
-  {
-    description = "Your nix flake";
-
-    inputs = {
-      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-      nixarr.url = "github:rasmus-kirk/nixarr";
-    };
-
-    outputs = { 
-      nixpkgs,
-      nixarr,
-      ...
-    }@inputs: {
-      nixosConfigurations = {
-        nixos = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-
-          modules = [
-            ./nixos/servarr/configuration.nix
-            nixarr.nixosModules.default
-          ];
-
-          specialArgs = { inherit inputs; };
-        };
+{
+  description = "Media Server with Nixarr";
+  
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixarr.url = "github:rasmus-kirk/nixarr";
+  };
+  
+  outputs = { nixpkgs, nixarr, ... }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          nixarr.nixosModules.default
+        ];
       };
     };
-  }
+  };
+}
